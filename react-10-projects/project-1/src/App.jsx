1c2bbb6793490ac6993478a31456a8190a0a14db
WaliurRahman450 <waliurrahaman450@gmail.com>
2024-05-04 15:31:30 +0600

    done

---
+++ react-10-projects/project-1/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Hero from './Components/Hero'
+import Navigation from './Components/Navigation'
+
+function App() {
+  return (
+    <>
+      <Navigation />
+      <Hero />
+      <div className="footer">
+        <p><small>All rights reserved by Waliur Rahman, 2024</small></p>
+      </div>
+    </>
+
+  )
+}
+
+export default App
